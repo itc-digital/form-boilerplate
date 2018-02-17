dfb0c979325e22db8ef58c11e87ed600112022ba
Mikhail Malyshev <mlshv42@yandex.ru>
2018-02-17 11:04:32 +0300

    Добавлена демонстрация передачи данных от PhoneInput в App (#4)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import PhoneInput from './PhoneInput';
 
 class App extends Component {
+    state = {
+        phoneNumber: ''
+    };
+
+    handleChange = event => this.setState({ phoneNumber: event.target.value });
+
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
+                <p>Телефон: {this.state.phoneNumber}</p>
                 <p>
-                    <PhoneInput />
+                    <PhoneInput onChange={this.handleChange} />
                 </p>
             </div>
         );
